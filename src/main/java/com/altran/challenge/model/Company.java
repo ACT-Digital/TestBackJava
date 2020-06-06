b25db894ff72120a054d0e0330cc9bbc009a5598
plaraujo <plaraujo@orizon.com.br>
2020-06-06 17:19:45 -0300

    adding company entity

---
+++ src/main/java/com/altran/challenge/model/Company.java
@@ -0,0 +1,34 @@
+package com.altran.challenge.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Company {
+
+    @Id
+    @SequenceGenerator(name = "companySequence", sequenceName = "company_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "companySequence")
+    @Column(length = 6)
+    private Integer id;
+
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 16)
+    private String login;
+
+    @Column(length = 16)
+    private String password;
+
+    @OneToMany(mappedBy = "company")
+    private List<Customer> customers;
+
+}
