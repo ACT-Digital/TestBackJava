33ecd205423a7d1870f67ffe95157eb3c4655c13
plaraujo <plaraujo@orizon.com.br>
2020-06-06 23:40:10 -0300

    updating package

--- src/main/java/com/altran/santander/challenge/config/SpringFoxConfig.java
+++ src/main/java/com/altran/santander/challenge/config/SpringFoxConfig.java
@@ -17,7 +17,7 @@ public class SpringFoxConfig {
 
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.altran.challenge.controller"))
+            .apis(RequestHandlerSelectors.basePackage("com.altran.santander.challenge.controller"))
             .paths(PathSelectors.any())
             .build();
 
