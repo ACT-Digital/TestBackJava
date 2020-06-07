63d85c1d9f82b56f327bbb8f2444a19d730f164d
plaraujo <plaraujo@orizon.com.br>
2020-06-07 17:17:19 -0300

    fixing swagger-ui

--- src/main/java/com/altran/santander/challenge/config/SpringFoxConfig.java
+++ src/main/java/com/altran/santander/challenge/config/SpringFoxConfig.java
@@ -2,14 +2,18 @@ package com.altran.santander.challenge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+/** Swagger Configuration */
 @Configuration
 @EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class SpringFoxConfig {
 
     @Bean
