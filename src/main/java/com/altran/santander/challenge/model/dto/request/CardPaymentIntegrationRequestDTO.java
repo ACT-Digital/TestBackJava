96f5cf9f32c07111a551626315e494b9099658b2
plaraujo <plaraujo@orizon.com.br>
2020-06-08 01:51:55 -0300

    new dtos

---
+++ src/main/java/com/altran/santander/challenge/model/dto/request/CardPaymentIntegrationRequestDTO.java
@@ -0,0 +1,30 @@
+package com.altran.santander.challenge.model.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardPaymentIntegrationRequestDTO implements Serializable {
+
+    @JsonProperty("descricao")
+    private String description;
+
+    @JsonProperty("valor")
+    private Double value;
+
+    @JsonProperty("codigousuario")
+    private Integer costumerId;
+
+    @JsonProperty("data")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Calendar date;
+
+}
