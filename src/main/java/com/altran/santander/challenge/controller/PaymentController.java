537fde9d2d83dbb17e4af3487a5e6e25bbd20dcb
plaraujo <plaraujo@orizon.com.br>
2020-06-08 01:52:45 -0300

    creating new functionalities

--- src/main/java/com/altran/santander/challenge/controller/PaymentController.java
+++ src/main/java/com/altran/santander/challenge/controller/PaymentController.java
@@ -1,17 +1,21 @@
 package com.altran.santander.challenge.controller;
 
 import com.altran.santander.challenge.model.Payment;
+import com.altran.santander.challenge.model.dto.request.CardPaymentIntegrationRequestDTO;
+import com.altran.santander.challenge.model.dto.request.EditCategoryRequestDTO;
 import com.altran.santander.challenge.model.dto.request.PaymentRequestDTO;
 import com.altran.santander.challenge.model.dto.response.IdResponseDTO;
 import com.altran.santander.challenge.model.dto.response.PaymentResponseDTO;
 import com.altran.santander.challenge.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,9 +37,10 @@ public class PaymentController {
 
     }
 
-    /** Funcionalidade: Integração de gastos por cartão */
     @PostMapping
-    public ResponseEntity<IdResponseDTO> save(@RequestBody PaymentRequestDTO paymentRequestDTO, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<IdResponseDTO> save(
+            @RequestBody PaymentRequestDTO paymentRequestDTO,
+            UriComponentsBuilder uriBuilder) {
 
         Payment payment = paymentService.saveRequest(paymentRequestDTO);
         URI uri = uriBuilder.build("api/payment");
@@ -43,12 +48,64 @@ public class PaymentController {
 
     }
 
-    /** Funcionalidade: Filtro de gastos (Fazer pesquisa a partir do parâmetro data) */
+    @PostMapping("/card-payment-integration")
+    public ResponseEntity<IdResponseDTO> cardPaymentIntegration(
+            @RequestBody CardPaymentIntegrationRequestDTO cardPaymentIntegrationRequestDTO,
+            UriComponentsBuilder uriBuilder) {
 
-    /** Funcionalidade: Listagem de gastos (Listar ordenando dados do mais recente para o mais antigo) */
+        Payment payment = paymentService.saveIntegration(cardPaymentIntegrationRequestDTO);
+        URI uri = uriBuilder.build("api/payment");
+        return ResponseEntity.created(uri).body(new IdResponseDTO(payment.getId()));
 
-    /** Funcionalidade: Categorização de gastos (Possibilidade de editar a descrição do pagamento somente quando não existir) */
+    }
 
+    @GetMapping("/list-by-recent")
+    public ResponseEntity<List<PaymentResponseDTO>> findAllByRecent() {
 
+        return ResponseEntity.ok(paymentService.findAllByRecent()
+                .stream()
+                .map(PaymentResponseDTO::new)
+                .collect(Collectors.toList()));
+
+    }
+
+    @GetMapping("/search-by-date/{date}")
+    public ResponseEntity<List<PaymentResponseDTO>> findAllByDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Calendar date) {
+
+        if (!paymentService.existsPaymentDate(date)) {
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        return ResponseEntity.ok(paymentService.findAllByDate(date)
+                .stream()
+                .map(PaymentResponseDTO::new)
+                .collect(Collectors.toList()));
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PaymentResponseDTO> editCategory(
+            @RequestBody EditCategoryRequestDTO editCategoryRequestDTO,
+            @PathVariable Integer id) {
+
+        if (!paymentService.existsPaymentId(id)) {
+
+            return ResponseEntity.notFound().build();
+
+        }
+
+        Payment payment = paymentService.editCategory(editCategoryRequestDTO, id);
+
+        if(payment.getCategory() != editCategoryRequestDTO.getCategory()) {
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        return ResponseEntity.ok(new PaymentResponseDTO(payment));
+
+    }
 
 }
--- src/main/java/com/altran/santander/challenge/service/PaymentService.java
+++ src/main/java/com/altran/santander/challenge/service/PaymentService.java
@@ -2,11 +2,14 @@ package com.altran.santander.challenge.service;
 
 import com.altran.santander.challenge.model.Customer;
 import com.altran.santander.challenge.model.Payment;
+import com.altran.santander.challenge.model.dto.request.CardPaymentIntegrationRequestDTO;
+import com.altran.santander.challenge.model.dto.request.EditCategoryRequestDTO;
 import com.altran.santander.challenge.model.dto.request.PaymentRequestDTO;
 import com.altran.santander.challenge.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -36,7 +39,83 @@ public class PaymentService {
         Customer customer = customerService.findById(paymentRequestDTO.getCostumerId());
         payment.setCustomer(customer);
 
+        setCategoryAutomatically(payment, customer);
+
         return this.save(payment);
+
+    }
+
+    public Payment saveIntegration(CardPaymentIntegrationRequestDTO cardPaymentIntegrationRequestDTO) {
+
+        Payment payment = new Payment(cardPaymentIntegrationRequestDTO);
+        Customer customer = customerService.findById(cardPaymentIntegrationRequestDTO.getCostumerId());
+        payment.setCustomer(customer);
+
+        setCategoryAutomatically(payment, customer); //se n der certo jogar na classe
+
+        return this.save(payment);
+
+    }
+
+    /** Find all payments ordering by most recent date */
+    public List<Payment> findAllByRecent() {
+
+        return paymentRepository.findAllByOrderByDateDesc();
+
+    }
+
+    /** Find all payments based on the parameter date */
+    public List<Payment> findAllByDate(Calendar date) {
+
+        return paymentRepository.findAllByDate(date);
+
+    }
+
+    /** Method for user allow user to edit empty payment category */
+    public Payment editCategory(EditCategoryRequestDTO editCategoryRequestDTO, Integer id) {
+
+        Payment payment = paymentRepository.findById(id).get();
+
+        if (payment.getCategory() != null) {
+
+            return payment;
+
+        }
+
+        payment.setCategory(editCategoryRequestDTO.getCategory());
+
+        return this.save(payment);
+
+    }
+
+    /** Method used to check if exist a payment with the parameter date */
+    public boolean existsPaymentDate(Calendar date) {
+
+        return paymentRepository.existsPaymentByDate(date);
+
+    }
+
+    /** Method used to check if exist a payment with the parameter id */
+    public boolean existsPaymentId(Integer id) {
+
+        return paymentRepository.existsPaymentById(id);
+
+    }
+
+    /** Set category based on consumer descriptions */
+    public void setCategoryAutomatically(Payment payment, Customer customer) {
+
+        if (payment.getDescription() != null) {
+
+            String category = paymentRepository.categoryAutoSearch(payment.getDescription(), customer.getId());
+
+            if (category != null && category != "null") {
+
+                payment.setCategory(category);
+
+            }
+
+        }
 
     }
 
