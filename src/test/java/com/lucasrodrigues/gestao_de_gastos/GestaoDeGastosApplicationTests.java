41e39c39242dc96aed8c183eb22a6c97f91196d0
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-11 06:49:45 -0300

    projeto gestao de gastos criado

---
+++ src/test/java/com/lucasrodrigues/gestao_de_gastos/GestaoDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrodrigues.gestao_de_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
