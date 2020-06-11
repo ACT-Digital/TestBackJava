ac7084b111f17255dec1691fedfbb6f952f10750
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-11 09:20:52 -0300

    Criando o Document Usuario, UsuarioCommandRepositoy e o DataSeeding na TestConfig para ter coleções necessárias para fazer os testes no BD

--- src/main/java/com/lucasrodrigues/gestao_de_gastos/configs/TestConfig.java
+++ src/main/java/com/lucasrodrigues/gestao_de_gastos/configs/TestConfig.java
@@ -1,8 +1,15 @@
 package com.lucasrodrigues.gestao_de_gastos.configs;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.lucasrodrigues.gestao_de_gastos.commands.repository.UsuarioCommandRepository;
+import com.lucasrodrigues.gestao_de_gastos.domian.Usuario;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,11 +19,26 @@ import io.swagger.annotations.ApiOperation;
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 
+	
+	private UsuarioCommandRepository userCommandRepo;
+	
+	@Autowired
+	public TestConfig(UsuarioCommandRepository userCommandRepo) {
+		this.userCommandRepo = userCommandRepo;
+	}
+	
 	@ApiOperation(value = "Metodo executado quando a aplicação é iniciada")
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		userCommandRepo.saveAll(getListUsuarios());
 		
 	}
 
+	private List<Usuario> getListUsuarios(){
+		Usuario u1 = new Usuario(null, "leao", "leao@gmail", "1234");
+		Usuario u2 = new Usuario(null, "bruno", "bruno@gmail", "1296");
+		
+		return Arrays.asList(u1,u2);
+		
+	}
 }
