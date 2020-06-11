d7ebbb045581050ee9e6a419312a3da357d83304
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-11 10:11:43 -0300

    padrao DTO, para customizar os atributos, evitar que dados exclusivos do sistema sejam espostos e otimizar o trafego

---
+++ src/main/java/com/lucasrodrigues/gestao_de_gastos/querys/DTO/UsuarioQueryDTO.java
@@ -0,0 +1,71 @@
+package com.lucasrodrigues.gestao_de_gastos.querys.DTO;
+
+import java.io.Serializable;
+
+import com.lucasrodrigues.gestao_de_gastos.domian.Usuario;
+
+public class UsuarioQueryDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String codigo;
+	private String nome;
+	private String email;
+	
+	public UsuarioQueryDTO(Usuario obj) {
+		this.codigo = obj.getCodigo();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioQueryDTO other = (UsuarioQueryDTO) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+	
+}
--- src/main/java/com/lucasrodrigues/gestao_de_gastos/querys/resources/UsuarioQueryResource.java
+++ src/main/java/com/lucasrodrigues/gestao_de_gastos/querys/resources/UsuarioQueryResource.java
@@ -1,6 +1,7 @@
 package com.lucasrodrigues.gestao_de_gastos.querys.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lucasrodrigues.gestao_de_gastos.domian.Usuario;
+import com.lucasrodrigues.gestao_de_gastos.querys.DTO.UsuarioQueryDTO;
 import com.lucasrodrigues.gestao_de_gastos.querys.services.UsuarioQueryService;
 
 import io.swagger.annotations.Api;
@@ -28,9 +29,9 @@ public class UsuarioQueryResource {
 	
 	@ApiOperation(value = "Retorna uma lista com todos os usuarios")
 	@GetMapping
-	public ResponseEntity<List<Usuario>> findAll(){
+	public ResponseEntity<List<UsuarioQueryDTO>> findAll(){
 		
-		List<Usuario> result = userQueryService.findAll();
+		List<UsuarioQueryDTO> result = userQueryService.findAll().stream().map( x -> new UsuarioQueryDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(result);
 	}
 }
